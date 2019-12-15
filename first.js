e2826f2da4074aa62cd4aacda717f5ea5d2ed691
Charles Wanjohi <cwanjohi@kenya.cellulant.com>
2019-12-15 11:09:55 +0300

    add first.js file

---
+++ first.js
@@ -0,0 +1,9 @@
+
+var name='charles';
+var home="nayahururu";
+console.log(name);
+const array = ["one", "two", "three"];
+array.forEach(function (item, index) {
+  console.log(item, index);
+});
+const x = (x, y) => x * y;
